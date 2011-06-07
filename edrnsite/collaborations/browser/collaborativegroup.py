2509a5506f61c4ee76e71257bd3d8da93e1fe8be
Sean Kelly <sean.c.kelly@me.com>
2011-06-07 04:25:30 +0000

    View methods to fetch top-3 and totals of biomarkers and protocols

--- edrnsite/collaborations/browser/collaborativegroup.py
+++ edrnsite/collaborations/browser/collaborativegroup.py
@@ -8,18 +8,40 @@
 from Acquisition import aq_inner
 # from edrnsite.collaborations import PackageMessageFactory as _
 # from edrnsite.collaborations.interfaces import
-# from plone.memoize.instance import memoize
 # from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.memoize.instance import memoize
 import urllib
+
+_top = 3
 
 class CollaborativeGroupView(BrowserView):
     '''Default view for a Collaborative Group.'''
-    __call__ = ViewPageTemplateFile('templates/collaborativegroup.pt')
+    index = ViewPageTemplateFile('templates/collaborativegroup.pt')
+    def __call__(self):
+        return self.index()
     def facebookURL(self):
         context = aq_inner(self.context)
         return u'http://facebook.com/sharer.php?' + urllib.urlencode(dict(t=context.title, u=context.absolute_url()))
     def twitterURL(self):
         context = aq_inner(self.context)
         return u'http://twitter.com/share?' + urllib.urlencode(dict(text=context.title, url=context.absolute_url()))
+    @memoize
+    def topBiomarkers(self):
+        context = aq_inner(self.context)
+        return context.biomarkers[0:_top]
+    @memoize
+    def topProtocols(self):
+        context = aq_inner(self.context)
+        return context.protocols[0:_top]
+    @memoize
+    def numBiomarkers(self):
+        context = aq_inner(self.context)
+        return len(context.biomarkers)
+    @memoize
+    def numProtocols(self):
+        context = aq_inner(self.context)
+        return len(context.protocols)
+    def numTops(self):
+        return _top
