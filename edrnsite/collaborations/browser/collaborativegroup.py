f036bc30443c85de10a3e952e3ea80c1f5285216
Sean Kelly <sean.c.kelly@me.com>
2011-06-21 20:05:17 +0000

    Add view methods to retrieve current+future events and past events, along with conditionals on existence of each

--- edrnsite/collaborations/browser/collaborativegroup.py
+++ edrnsite/collaborations/browser/collaborativegroup.py
@@ -6,12 +6,12 @@
 '''
 
 from Acquisition import aq_inner
-# from edrnsite.collaborations import PackageMessageFactory as _
-# from edrnsite.collaborations.interfaces import
+from DateTime import DateTime
+from plone.memoize.instance import memoize
+from Products.ATContentTypes.interface.event import IATEvent
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.memoize.instance import memoize
 import urllib
 
 _top = 3
@@ -19,6 +19,7 @@ _top = 3
 # Why aren't these permission names defined as constants somewhere in ATContentTypes?
 _addableContent = {
     # Add type   Permission name                    Type name
+    'event':    ('ATContentTypes: Add Event',       'Event'),
     'file':     ('ATContentTypes: Add File',        'File'),
     'image':    ('ATContentTypes: Add Image',       'Image'),
     'page':     ('ATContentTypes: Add Document',    'Document'),
@@ -67,5 +68,19 @@ class CollaborativeGroupView(BrowserView):
         return mtool.checkPermission(_addableContent[buttonType][0], context)
     def newButtonLink(self, buttonType):
         return aq_inner(self.context).absolute_url() + '/createObject?type_name=' + _addableContent[buttonType][1]
+    def haveEvents(self):
+        return len(self.currentEvents()) > 0
+    def havePastEvents(self):
+        return len(self.pastEvents()) > 0
+    @memoize
+    def currentEvents(self):
+        return self._getEvents(end={'query': DateTime(), 'range': 'min'})
+    @memoize
+    def pastEvents(self):
+        return self._getEvents(end={'query': DateTime(), 'range': 'max'})
+    def _getEvents(self, **criteria):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(object_provides=IATEvent.__identifier__, sort_on='start', **criteria)
+        return [dict(title=i.Title, description=i.Description, start=i.start, url=i.getURL()) for i in results]
     
-
