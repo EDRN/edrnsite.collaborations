3fa226a3eca4edcd9873e6c577c73cbd67163da8
Sean Kelly <kelly@seankelly.biz>
2016-11-06 11:21:26 -0600

    bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os.path
 # ------------
 
 _name        = 'edrnsite.collaborations'
-_version     = '1.0.3'
+_version     = '1.0.4'
 _description = 'EDRN Collaborative Group Workspaces'
 _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
